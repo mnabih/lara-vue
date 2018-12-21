ce1425e406b4950dd4d8dec07f58309b03866cc3
mnm <mnm_cc9111@yahoo.com>
2018-12-21 08:51:33 +0200

    vue-router

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,29 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueRouter from 'vue-router'
+import dashboardPage from './components/dashboard.vue'
+import profilePage from './components/profile.vue'
+
+Vue.use(VueRouter)
+
+
+let routes = [
+  { path: '/dashboard', component: dashboardPage },
+  { path: '/profile', component: profilePage }
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+// 4. Create and mount the root instance.
+// Make sure to inject the router with the router option to make the
+// whole app router-aware.
+// const app = new Vue({
+//   router
+// }).$mount('#app')
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -29,5 +52,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 });
